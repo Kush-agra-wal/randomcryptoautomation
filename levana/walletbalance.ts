d65008ec6c4c7dce6eac93bf82ade91082376fae
Kush-agra-wal <79041696+Kush-agra-wal@users.noreply.github.com>
2023-06-05 13:35:44 -0700

    Update walletbalance.ts

--- levana/walletbalance.ts
+++ levana/walletbalance.ts
@@ -1,19 +1,19 @@
 import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate"
 import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing"
 
-const rpcEndpoint = "https://rpc-v4-udb8dydv.dragonfire.sandbox.levana.finance"
-const marketAddress =
-  "levana1hy98vz70fmur9pfyjunet9cr7yqx3jddl24g9jmaunr7mdxuwn8s5s6gj8"
+const rpcEndpoint = "https://sei.kingnodes.com/"
+const factoryAddress =
+  "sei1aldsk8lmc9z3q0ax47zgcluxx4x44ncf0nwnvtdepedg07jqqnrq0uel80"
 const seedPhrase =
-  "hen punch extra relax craft bicycle iron core purity tissue talk impact kitchen inhale slush hip amateur during ranch inspire much correct century where"
+  "gloom human walnut bright steel private faith bonus wisdom fly sword pipe"
 
 const runAll = async () => {
   const signer = await DirectSecp256k1HdWallet.fromMnemonic(seedPhrase, {
     // This is the prefix used by the Dragonfire testnet
-    prefix: "levana"
+    prefix: "sei"
   })
 
-  // Get the address of the account. This should match what you see inside Keplr
+  // Get the address of the account. This should match what you see inside leap
   const accounts = await signer.getAccounts()
   const address = accounts[0].address
   console.log(`Wallet address is ${address}`)
@@ -22,22 +22,19 @@ const runAll = async () => {
     rpcEndpoint,
     signer
   )
-
+  const { market_addr: marketAddress } = await client.queryContractSmart(
+    factoryAddress,
+    {
+      market_info: { market_id: "ETH_USD" },
+    }
+  );
+  console.log(marketAddress)
   // And print some information about this account
   const account = await client.getAccount(address)
   console.log(`Account information: ${JSON.stringify(account)}`)
-  const balance = await client.getBalance(address, "udragonfire")
+  const balance = await client.getBalance(address, "sei")
   console.log(`Balance is ${balance.amount}${balance.denom}`)
-  const status = await client.queryContractSmart(marketAddress, {
-    status: {}
-  })
-  const collateralAddress = status.collateral.cw20.addr
-  const walletAddress = accounts[0].address
-  const balance2 = await client.queryContractSmart(collateralAddress, {
-    balance: { address: walletAddress }
-  })
-  console.log('CW20 (divide by 10^6 for atom tokens) :')
-  console.log(balance2)
+  
 }
 
 runAll()
